ffe4998083f6b227fb02e3426e49d2dbe7c25c8d
Hifune <design.ko@mail.ru>
2022-12-13 20:29:19 +0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry:{
+        main: path.resolve(__dirname,'./src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './production'),
+        filename: 'bundje.js',
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'База знаний',
+            template: path.resolve(__dirname, './src/template.html'), //шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+    module: {
+        rules:[
+            // javascript
+           {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+          },
+            // Изображения
+             {
+             test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+             type: 'asset/resource',
+             },
+            // шрифты и svg
+            {
+            test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+            type: 'asset/inline',
+            }, 
+             // css, PostCSS, Sass
+            {
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader'],
+            }, 
+        ]
+    }
+}
+
